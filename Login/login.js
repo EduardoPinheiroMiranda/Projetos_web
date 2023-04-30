0cc7e3cee870d4a150bdf13cafba6a7d3fe7d0d1
Eduardo Pinheiro Miranda <89249898+EduardoPinheiroMiranda@users.noreply.github.com>
2023-04-30 12:28:00 -0300

    add transição de elementos

--- Login/login.js
+++ Login/login.js
@@ -4,15 +4,18 @@ const instrucao = document.querySelectorAll(".instruction")
 const criarConta = document.querySelector("#criarConta")
 const login = document.querySelector("#login")
 
-criarConta.onclick = () => {
-    moverInstrucao[1].classList.add("mover")
-    instrucao[1].classList.add("ocultarInstrucao")
-    instrucao[0].classList.add("mostrarInformacao")
-}
 
 login.onclick = () => {
-    moverInstrucao[1].classList.remove("mover")
-    instrucao[1].classList.remove("ocultarInstrucao")
-
+    moverInstrucao[1].classList.add("mover")
+    instrucao[0].classList.add("ocultarInstrucao")
+    instrucao[1].classList.remove("display")
+    
+    
 }
 
+criarConta.onclick = () => {
+    moverInstrucao[1].classList.remove("mover")
+    instrucao[0].classList.remove("ocultarInstrucao")
+    instrucao[1].classList.add("display")
+
+}
